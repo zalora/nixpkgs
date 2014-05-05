e3e76412b6a8baaa5a480e64ad7f4df3cae50524
Shea Levy <shea@shealevy.com>
2014-05-05 14:58:51 -0400

    Get all lib functions from lib, not pkgs.lib, in modules

--- nixos/modules/services/desktops/gnome3/gnome-online-accounts.nix
+++ nixos/modules/services/desktops/gnome3/gnome-online-accounts.nix
@@ -1,8 +1,8 @@
 # GNOME Online Accounts daemon.
 
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
-with pkgs.lib;
+with lib;
 
 {
 
--- nixos/modules/services/desktops/gnome3/seahorse.nix
+++ nixos/modules/services/desktops/gnome3/seahorse.nix
@@ -1,8 +1,8 @@
 # Seahorse daemon.
 
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
-with pkgs.lib;
+with lib;
 
 {
 
