739911fe1208d632bf17c6df72abbeaf9f5a3a8d
Igor Pashev <pashev.igor@gmail.com>
2015-05-27 06:25:16 +0300

    python 2.7 from upstream

--- pkgs/development/interpreters/python/2.7/default.nix
+++ pkgs/development/interpreters/python/2.7/default.nix
@@ -1,7 +1,19 @@
-{ stdenv, fetchurl, zlib ? null, zlibSupport ? true, bzip2, includeModules ? false
-, sqlite, tcl, tk, x11, openssl, readline, db, ncurses, gdbm, libX11, self, callPackage }:
+{ stdenv, fetchurl, self, callPackage
+, bzip2, openssl
+
+, includeModules ? false
+
+, db, gdbm, ncurses, sqlite, readline
+
+, tcl ? null, tk ? null, x11 ? null, libX11 ? null, x11Support ? false
+, zlib ? null, zlibSupport ? true
+}:
 
 assert zlibSupport -> zlib != null;
+assert x11Support -> tcl != null
+                  && tk != null
+                  && x11 != null
+                  && libX11 != null;
 
 with stdenv.lib;
 
@@ -28,7 +40,7 @@ let
       # if DETERMINISTIC_BUILD env var is set
       ./deterministic-build.patch
     ];
-    
+
   preConfigure = ''
       # Purity.
       for i in /usr /sw /opt /pkg; do
@@ -46,9 +58,15 @@ let
       touch $out/include/python${majorVersion}/pyconfig.h
     '';
 
+  configureFlags = "--enable-shared --with-threads --enable-unicode=ucs4";
+
   buildInputs =
     optional (stdenv ? cc && stdenv.cc.libc != null) stdenv.cc.libc ++
-    [ bzip2 openssl ] ++ optionals includeModules [ db openssl ncurses gdbm libX11 readline x11 tcl tk sqlite ]
+    [ bzip2 openssl ]
+    ++ optionals includeModules (
+        [ db gdbm ncurses sqlite readline
+        ] ++ optionals x11Support [ tcl tk x11 libX11 ]
+    )
     ++ optional zlibSupport zlib;
 
   # Build the basic Python interpreter without modules that have
@@ -57,13 +75,12 @@ let
     name = "python-${version}";
     pythonVersion = majorVersion;
 
-    inherit majorVersion version src patches buildInputs preConfigure;
+    inherit majorVersion version src patches buildInputs preConfigure
+            configureFlags;
 
     LDFLAGS = stdenv.lib.optionalString (!stdenv.isDarwin) "-lgcc_s";
     C_INCLUDE_PATH = concatStringsSep ":" (map (p: "${p}/include") buildInputs);
     LIBRARY_PATH = concatStringsSep ":" (map (p: "${p}/lib") buildInputs);
-
-    configureFlags = "--enable-shared --with-threads --enable-unicode";
 
     NIX_CFLAGS_COMPILE = optionalString stdenv.isDarwin "-msse2";
     DETERMINISTIC_BUILD = 1;
@@ -87,7 +104,7 @@ let
         ln -s $out/share/man/man1/{python2.7.1.gz,python.1.gz}
 
         paxmark E $out/bin/python${majorVersion}
-        
+
         ${ optionalString includeModules "$out/bin/python ./setup.py build_ext"}
       '';
 
@@ -133,7 +150,7 @@ let
     if includeModules then null else stdenv.mkDerivation rec {
       name = "python-${moduleName}-${python.version}";
 
-      inherit src patches preConfigure;
+      inherit src patches preConfigure configureFlags;
 
       buildInputs = [ python ] ++ deps;
 
@@ -145,6 +162,7 @@ let
             'self.extensions = [ext for ext in self.extensions if ext.name in ["${internalName}"]]'
 
           python ./setup.py build_ext
+          [ -z "$(find build -name '*_failed.so' -print)" ]
         '';
 
       installPhase =
@@ -178,7 +196,7 @@ let
     crypt = buildInternalPythonModule {
       moduleName = "crypt";
       internalName = "crypt";
-      deps = [ ];
+      deps = optional (stdenv ? glibc) stdenv.glibc;
     };
 
     gdbm = buildInternalPythonModule {
@@ -192,10 +210,14 @@ let
       deps = [ sqlite ];
     };
 
+  } // optionalAttrs x11Support {
+
     tkinter = buildInternalPythonModule {
       moduleName = "tkinter";
       deps = [ tcl tk x11 libX11 ];
     };
+
+  } // {
 
     readline = buildInternalPythonModule {
       moduleName = "readline";
